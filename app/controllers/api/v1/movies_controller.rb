eba04bdeae8fa036ef77963df3453ea0787f90e5
John Blank <veloblank@gmail.com>
2020-01-07 19:49:40 -0500

    Add conditional logic to movies controller actions

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -7,28 +7,45 @@ class Api::V1::MoviesController < ApplicationController
 
   def show
     movie = Movie.find_by(:id => params[:id])
-    render json: movie
+    if movie
+      render json: movie, status: 200
+    else
+      render json: {message: "The requested movie could not be found."}
+    end
   end
   
   def create
-    movie = Movie.create(move_params)
-    render json: movie, status: 200
+    movie = Movie.new(movie_params)
+    if movie.valid?
+      movie.save
+      render json: movie, status: 200
+    else
+      render json: {message: "The resource could not be created."}
+    end
   end
   
   def update
     movie = Movie.find_by(:id => params[:id])
-    movie.update(movie_params)
+    if movie && movie.update(movie_params)
+      render json: movie, status: 200
+    else
+      render json: {message: "The requested movie resource could not be updated."}
+    end
   end
   
   def destroy
     movie = Movie.find_by(:id => params[:id])
-    movie.delete
-    render json: {movieId: movie.id}
-
+    if movie
+      movie.delete
+      render json: {movieId: movie.id}
+    else
+      render json: {message: "The requested resource could not be deleted."}
+    end
   end
+  
   private
   def movie_params
-    params.require(:movie).permit(:title)
+    params.require(:movie).permit(:title, :synopsis, :rating, :poster_src)
   end
 
 
